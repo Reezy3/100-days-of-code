d37a3f7f54fc90516e5c38ff60bddd61522ae581
Reezy3 <105237608+Reezy3@users.noreply.github.com>
2022-06-28 10:38:52 +0100

    Day_8

---
+++ day_8/start.py
@@ -0,0 +1,26 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("Welcome to day 8 of learning")
+    print("I'm Arinze and I will keep improving")
+    print("I hope to get really good at programming")   
+    
+greet()
+
+# Function that allows input
+# def greet_with_name(name):
+#     print(f"Welcome {name} to day 8 of learning")
+#     print(f"How do you do {name}?")
+    
+# greet_with_name("Ben")
+  
+  
+# Functions with more than one input
+def greet_with(name, location):
+    print(f"Hello {name} ")
+    print(f"What is it like in {location}?")
+
+greet_with("Arinze", "Nigeria")   
